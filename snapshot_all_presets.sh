31832afc6e8d430260554d6d6b49efd499bffc1e
Robert M. Toups, Jr <robert.toups@gmail.com>
2022-01-29 10:13:33 -0500

    Raised value to 20

--- snapshot_all_presets.sh
+++ snapshot_all_presets.sh
@@ -13,22 +13,33 @@ done
 
 mkdir -p ${IMAGE_DIR}
 SLEEP_SEC=5
-for (( i=1; i<=9; i++ ))
+for (( i=1; i<=20; i++ ))
 do
+  # if [ $i -lt 10 ]; then
+  #   NUM="0${i]"
+  # # else
+  # #   NUM="${i]}"
+  # fi
+  if [ $i -lt 10 ]; then
+    NUM="0${i}"
+  else
+    NUM=${i}
+  fi
 	echo "Moving to Preset #${i}"
 	curl "http://${CAMERA_IP}/cgi-bin/ptzctrl.cgi?ptzcmd&poscall&${i}"
 	sleep ${SLEEP_SEC}
-  IMAGE_PATH="${IMAGE_DIR}/${CAMERA_IP}-Preset_${i}-Snapshot.jpg"
+  IMAGE_PATH="${IMAGE_DIR}/${CAMERA_IP}-Preset_${NUM}-Snapshot.jpg"
   echo "Downloading Snapshot to ${IMAGE_PATH}"
 	curl -s -o ${IMAGE_PATH} "http://${CAMERA_IP}/snapshot.jpg"
-  LABEL_PATH="${IMAGE_DIR}/${CAMERA_IP}-Preset_${i}-Labeled.jpg"
+  LABEL_PATH="${IMAGE_DIR}/${CAMERA_IP}-Preset_${NUM}-Labeled.jpg"
   echo "Labeling ${IMAGE_PATH} and saving to ${LABEL_PATH}"
   montage \
     -label "Preset #${i}" -font Nimbus-Sans-L-Bold-Condensed -pointsize 48 \
     ${IMAGE_PATH} \
     -geometry +0+5 \
     ${LABEL_PATH}
-  ICON_PATH="${IMAGE_DIR}/${CAMERA_IP}-Preset_${i}-Icon.jpg"
+
+  ICON_PATH="${IMAGE_DIR}/${CAMERA_IP}-Preset_${NUM}-Icon.jpg"
   echo "Making StreamDeck Icon: ${ICON_PATH}"
   convert ${IMAGE_PATH} -resize 72x72! ${ICON_PATH}
 done
@@ -36,7 +47,7 @@ done
 echo "Building Preset Image Grid"
 montage \
   -density 72 \
-  -tile 3x0 \
+  -tile 5x0 \
   -geometry +20+20 \
   -border 10 \
   -background black \
